9352e1966f43a28fd35fa05486c8187ea1abb562
Julia Sun <julia.sun1@gmail.com>
2016-11-26 17:28:43 -0500

    Add check for environment, makefile

---
+++ src/checktypes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ocamlbuild -pkgs oUnit,str,unix player.cmo game.cmo map.cmo test.byte
+if [[ $? -ne 0 ]]; then
+  cat <<EOF
+===========================================================
+WARNING
+
+Your code currently does not compile.  You will receive
+little to no credit for submitting this code. Check the
+error messages above carefully to determine what is wrong.
+See a consultant for help if you cannot determine what is
+wrong.
+===========================================================
+EOF
+  exit 1
+fi
