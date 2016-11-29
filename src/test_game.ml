ca8e9d8a7a84c03482752a4cbdcc0c975829bf57
Priyanka Boddu <pb448@cornell.edu>
2016-11-29 17:42:42 -0500

    started added test games

--- src/test_game.ml
+++ src/test_game.ml
@@ -4,6 +4,19 @@
 open OUnit2
 open Game
 
+let s = Yojson.Basic.from_file "simple_example.json"
+
+let init_s = s |> init_game
+
 let tests = [
+  "start" >::
+    (fun _ -> assert_equal 1 (let state = (init_game s) in state.start));
+
+  "start points" >:: (fun _ -> assert_equal 10000
+    (let state = (init_game s) in state.start_points));
+
+
+
+
 
 ]
